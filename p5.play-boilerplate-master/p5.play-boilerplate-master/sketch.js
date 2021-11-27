97758906d5a247b68bf3b76c9dddbc68a09e4f9a
arya2107 <69304191+arya2107@users.noreply.github.com>
2021-11-27 16:31:32 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,49 @@
+var hr,sec,min ,hrang,secang,minang
+function setup() {
+  createCanvas(400,400);
+ angleMode(DEGREES)
+}
+
+function draw() {
+  background(0);  
+ translate(200,200)
+ rotate(-90)
+ hr=hour()
+ sec=second()
+ min=minute()
+ hrang=map(hr%12,0,12,0,360)
+ secang=map(sec,0,60,0,360)
+ minang=map(min,0,60,0,360)
+ push()
+ rotate(secang)
+ stroke(255,0,0)
+ strokeWeight(7)
+ line(0,0,100,0)
+ pop()
+ push()
+ rotate(minang)
+ stroke(0,255,0)
+ strokeWeight(7)
+ line(0,0,75,0)
+ pop()
+ push()
+ rotate(hrang)
+ stroke(0,0,255)
+ strokeWeight(7)
+ line(0,0,50,0)
+ pop()
+ stroke(255,0,255)
+ point(0,0)
+ strokeWeight(10)
+ noFill()
+ stroke(255,0,0)
+ arc(0,0,300,300,0,secang)
+ stroke(0,255,0)
+ arc(0,0,280,280,0,minang)
+ stroke(0,0,255)
+ arc(0,0,260,260,0,hrang)
+
+ 
+
+
+}
